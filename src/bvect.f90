9908cd14efcd83067972a7986ef67172dcbed7b9
armnjfl <armnjfl@01623e51-5bed-4412-93bc-928f732d7874>
2013-10-16 12:13:08 +0000

    forgot to add back bvect.f90
    git-svn-id: file:///users/dor/armn/jfl/mon_svn/1D_SIM@96 01623e51-5bed-4412-93bc-928f732d7874

---
+++ src/bvect.f90
@@ -0,0 +1,31 @@
+subroutine bvect(tauair, un1, b)
+  use size
+  use resolution
+  use properties
+  use global_var
+
+  implicit none
+      
+  integer :: i
+  double precision h_at_u
+  double precision, intent(in) :: tauair(1:nx+1), un1(1:nx+1)
+  double precision, intent(out):: b(1:nx+1)
+
+  b(1)    = 0d0 ! close bc
+  b(nx+1) = 0d0 ! close bc
+
+  do i = 2, nx
+
+     h_at_u = ( h(i) + h(i-1) ) / 2d0
+
+     b(i) = tauair(i)  - ( P_half(i) - P_half(i-1) ) / Deltax +  &
+             ( rho * h_at_u * un1(i) ) / Deltat
+
+  enddo
+   
+  b(21)=b(21)-0.2d0
+  print *, 'cic', tauair(21), b(20), b(21)
+
+  return
+end subroutine bvect
+    
