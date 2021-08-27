625616d91c917549756d9e623e488297a2de5312
Damien Ringeisen <damien.ringeisen@gmail.com>
2021-08-27 16:26:07 -0400

    up utils

--- utils/plot1D.py
+++ utils/plot1D.py
@@ -7,63 +7,8 @@ from datetime import datetime
 plt.rc('text', usetex=True)
 plt.rc('font', family='sans')
 
-def fname_ex(pc,datas):
-    fname = str(datas['vrs'][pc[0]] + '_' + datas['ri'] + '_ts' + datas['tps'][pc[1]] + datas['px'])
-    return datas['fdr']+fname
+def loadxr_sNC(ds,name='', path=''):
 
-def loaddata(pc, datas):
-    fname = fname_ex(pc, datas)
-    data = np.loadtxt(fname)
-    return data
-
-def plotdatapc(data_pc, ax, x):
-    ax.plot(x, data_pc)
-    return None
-
-def mainplot(datas, pcs):
-    fig, ax = plt.subplots()
-    for i in range(len(pcs)):
-        data_pc = loaddata(pcs[i], datas)
-        x = np.linspace(0, 2000, len(data_pc))
-        plotdatapc(data_pc, ax, x)
-
-    ax.set_xlabel('x [km]')
-    ax.set_ylabel(str(datas['vrs'][pcs[0][0]]))
-    return None
-
-def plotloop(datas, pcsl):
-    for j in range(len(pcsl)):
-        mainplot(datas, pcsl[j])
-    return None
-
-# # plotchoice lists
-# pcsl=[ [[3,0,0,0],[3,0,1,0],[3,0,2,0]],
-#        [[0,0,0,0],[0,0,1,0],[0,0,2,0]],
-#        [[1,0,0,0],[1,0,1,0],[1,0,2,0]],
-#        [[4,0,0,0],[4,0,1,0],[4,0,2,0]],
-#      ]
-
-# plotloop(datas,pcsl)
-
-# plt.show()
-
-def loadxr_coords(datas):
-
-    data_pc = loaddata([0, 0], datas)
-    xc = np.linspace(0, 2e6, len(data_pc))
-    xg = (xc[1:] + xc[:-1]) / 2
-
-    tsps = np.array([int(i) for i in datas['tps']])
-
-    coords = dict(
-        timesteps=(["timestep"], tsps),
-        time=(["time"], tsps*datas['dt']),
-        xc= (["xc"], xc),
-        xg= (["xg"], xg)
-    )
-    return coords
-
-def saveNC(ds,name='', path=''):
     if name == '':
         name = ds.attrs['entry']['ri']+'.nc'
     if path == '':
@@ -77,13 +22,37 @@ def saveNC(ds,name='', path=''):
 
     return None
 
-def loadxr1D(datas, attrs={}, save=False):
+def loadxr_coords(d_vars):
 
-    coords = loadxr_coords(datas)
+    # Looking at datas for dimensions
+    nx = 0
+    for j in d_vars:
+        if d_vars[j].shape[1] > nx:
+            nx = d_vars[j].shape[1]
 
+    # Cell centers coordinates
+    xc = np.linspace(0, 2e6, nx)
+
+    # Cell Boundaries coordinates
+    xg = (xc[1:] + xc[:-1]) / 2
+
+    # Time coordinates
+    tsps = np.array([int(i) for i in datas['tps']])
+
+    # Create the coordinates dictionnary
+    coords = dict(
+        timesteps=(["timestep"], tsps),
+        time=(["time"], tsps*datas['dt']),
+        xc= (["xc"], xc),
+        xg= (["xg"], xg)
+    )
+
+    return coords
+
+def loadxr_data(datas):
+
+    # Create a dictionnary with all the variable at all timesteps
     d_vars = dict()
-
-    # create coords
     run = datas['ri']
     px = datas['px']
     for var in datas['vrs']:
@@ -95,50 +64,76 @@ def loadxr1D(datas, attrs={}, save=False):
             else:
                 d_vars[var] = np.array([data])
 
+    return d_vars
+
+def loadxr_SIM1D(datas, attrs={}, save=False):
+    # Extract the data from the files to a dictionnary
+    d_vars = loadxr_data(datas)
+
+    # Extract the coordinates for the data dictionnary
+    coords = loadxr_coords(d_vars)
+
+    #Create the data array dictionnary with the coordinates
     data_vars = dict()
     for var in d_vars:
         data_vars[var] = (['time', datas['vx'][var]], d_vars[var])
 
+    # Add import dictionnary to the attributes
     attrs['entry'] = datas
 
+    # Create the xarray dataset
     ds = xr.Dataset(
         data_vars=data_vars,
         coords=coords,
         attrs=attrs,
     )
 
+    # Save if asked
     if save:
-        saveNC(ds)
+        loadxr_sNC(ds)
 
+    #return the dataset
     return ds
 
-#available data in ../output/
-datas = {
-    'fdr': '../output/',
-    'vrs': ['A', 'h', 'div', 'Er', 'u'],
-    'vx': {'A': 'xc', 'h': 'xc', 'div': 'xc', 'Er': 'xg', 'u': 'xg'},
-    'ri': '00600s_010km_solv2_IMEX0_adv3_BDF20',
-    'tps': ['000001', '000100', '001440'],
-    'px': '.01',
-    'dt': 600
+def plot_SIM1D(ds,varn,tsts=[0,1],step=1):
 
-}
+    for var in varn:
+        plt.figure()
+        for i in range(int(tsts[0]*ds[var].shape[0]), int(tsts[1]*ds[var].shape[0]), step):
+            ds[var][i].plot(linestyle='dashed', label=str("t="+str(ds['time'][i].values) + " s"))
+        plt.legend()
+        plt.grid()
+        plt.title(str('variable ' + var))
 
-attrs = {'creation_date': datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
-         'author': 'Damien Ringeisen',
-         'email': 'damien.ringeisen@mcgill.ca'}
+    plt.show()
 
-ds = loadxr1D(datas, attrs=attrs, save=True)
+    return None
 
+##########################
 
+if __name__ == "__main__":
 
-# ds["Er"][0].plot(label=str("t="+str(ds['time'][0].values)+" s"))
-# ds["Er"][1].plot(label=str("t="+str(ds['time'][1].values)+" s"))
-# ds["Er"][2].plot(label=str("t="+str(ds['time'][2].values)+" s"))
+    # Exemple of use
 
-# ds["A"][0].plot(label=str("t="+str(ds['time'][0].values)+" s"))
-# ds["A"][1].plot(label=str("t="+str(ds['time'][1].values)+" s"))
-# ds["A"][2].plot(label=str("t="+str(ds['time'][2].values)+" s"))
+    #data to load in an xarray
+    datas = {
+        'fdr': '../output/',
+        'vrs': ['A', 'h', 'div', 'Er', 'u'],
+        'vx': {'A': 'xc', 'h': 'xc', 'div': 'xc', 'Er': 'xg', 'u': 'xg'},
+        'ri': '00600s_010km_solv1_IMEX0_adv3_BDF20',
+        'tps': ['000001', '000100', '001440'],
+        'px': '.01',
+        'dt': 600
+    }
 
-# plt.legend()
-# plt.show()
+    # attribute if the NC array is to be shared (Optional)
+    attrs = {'creation_date': datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+             'author': 'Damien Ringeisen',
+             'email': 'damien.ringeisen@mcgill.ca'}
+
+    # Loading all datas specified in "datas" as a xarray dataset
+    ds = loadxr_SIM1D(datas, attrs=attrs, save=True)
+
+    # Plotting for one dataset
+    varn = ["u", "h", "A", "div", "Er"]
+    plot_SIM1D(ds, varn)
